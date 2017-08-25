c496331a197d522f52b8b721839269faa916ca3f
WodenTzeen <tzeentchi@hotmail.fr>
2017-08-25 18:02:03 +0200

    put
    put file on github

---
+++ whoCheckCV.bash
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+###########
+# Dev by  : W0d3n
+# Date    : August 2017
+###########
+#
+#Find who see my cv on my website
+
+ip=$(cat /var/log/nginx/access.log | grep "cv.html" | tr ' ' '@' | cut -d '@' -f1 | uniq)
+date > rapport.txt
+for i in $(echo $ip); do echo -e "###############\n$i\n" && whois $i | grep -e descr -e role -e Org && echo -e "\n"; done
+
