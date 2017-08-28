afbb94c72e79090dee663d2b7ed9b9884b2e8667
WodenTzeen <tzeentchi@hotmail.fr>
2017-08-28 10:33:30 +0200

    last update

--- CheckIP/whoCheckCV.sh
+++ CheckIP/whoCheckCV.sh
@@ -8,5 +8,5 @@
 #Find who see my cv on my website
 
 ip=$(cat /var/log/nginx/access.log | grep "cv.html" | tr ' ' '@' | cut -d '@' -f1 | uniq)
-for i in $(echo $ip); do date && echo -e "###############\n$i\n" && whois $i | grep -e descr -e role -e Org && echo -e "\n"; done
+for i in $(echo $ip); do date && echo -e "###############\n$i\n" && whois $i | egrep "descr|role|Org" && echo -e "\n"; done
 
